6de90346137179be30e97c38d4482e0d4ead8558
William Cross <WilliamCross.CS@gmail.com>
2024-10-14 15:59:41 -0400

    Change code to center canvas

--- sketch.js
+++ sketch.js
@@ -3,28 +3,37 @@ let inputClockInMinute;
 let inputHourlyWage;
 let checkPM;
 
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  inputClockInHour.position(width / 6 - 35, height / 6 * 5 - 50);
+  checkPM.position(width / 6 + 25, height / 6 * 5 - 47);
+  inputClockInMinute.position((width / 6) * 3 - 25, height / 6 * 5  -50);
+  inputHourlyWage.position((width / 6) * 5 - 25, height / 6 * 5 -50);
+}
+
 function setup() {
-  createCanvas(400, 280);
+  createCanvas(windowWidth, windowHeight);
   inputClockInHour = createInput("");
   inputClockInHour.style("width", "50px");
   inputClockInHour.style("text-align", "center");
-  inputClockInHour.position(width / 6 - 35, 210);
+  inputClockInHour.position(width / 6 - 35, height / 6 * 5 - 50);
   
   
   checkPM = createCheckbox('PM');
-  checkPM.position(width / 6 + 25, 210);
+  checkPM.position(width / 6 + 25, height / 6 * 5 - 47);
   
   
   inputClockInMinute = createInput("");
   inputClockInMinute.style("width", "50px");
   inputClockInMinute.style("text-align", "center");
-  inputClockInMinute.position((width / 6) * 3 - 25, 210);
+  inputClockInMinute.position((width / 6) * 3 - 25, height / 6 * 5  -50);
   
   
   inputHourlyWage = createInput("");
   inputHourlyWage.style("width", "50px");
   inputHourlyWage.style("text-align", "center");
-  inputHourlyWage.position((width / 6) * 5 - 25, 210);
+  inputHourlyWage.position((width / 6) * 5 - 25, height / 6 * 5 -50);
   textAlign(CENTER);
   textFont("Bungee");
 }
@@ -33,19 +42,19 @@ function draw() {
   background(194, 225, 194);
   textSize(15);
   fill(204, 71, 71);
-  text("Money made since you've clocked in:", width / 2, 50);
+  text("Money made since you've clocked in:", width / 2, height / 6);
   textSize(75);
   fill(51, 115, 87);
   text(
     calculateMoney(int(inputClockInHour.value()), int(inputClockInMinute.value())),
     width / 2,
-    155
+    height / 2
   );
   textSize(12);
   fill(45, 42, 50);
-  text("Clock In Hour", width / 6, 250);
-  text("Clock In Minute", (width / 6) * 3, 250);
-  text("Hourly Wage", (width / 6) * 5, 250);
+  text("Clock In Hour", width / 6, height / 6 * 5);
+  text("Clock In Minute", (width / 6) * 3, height / 6 * 5);
+  text("Hourly Wage", (width / 6) * 5, height / 6 * 5);
 }
 
 function calculateMoney(hours, minutes) {
